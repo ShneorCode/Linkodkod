0ea2955a29ce1ab5e54d90f01e00680438d931df
Shneor <mm4471661@gmail.com>
2025-09-10 17:52:59 +0300

    Game over

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import routerPosts from "./routes/postsRoutes.js";
+import cors from 'cors';
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import './css/App.css'
 import {Homepage} from './components/Homepage'
 import '../src/css/PostCard.css'
 import '../src/css/Homepage.css'
+import { useContext } from 'react'
+import { PostConetext, useCheckPost } from './context/Post'
 
 
 
 function App() {
-
+  const {posts, addPost} = useCheckPost(PostConetext)
   return (
     <>
     <Homepage />
